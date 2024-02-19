9015fca6aeafb5bc4702dceb0514ff060b767259
vr1 <hivr1eva@gmail.com>
2024-02-19 12:42:25 -0500

    add assistant thread form

---
+++ src/components/thread.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+    newMessage: z.string().min(2, {
+        message: "Thread message must be at least 2 characters.",
+    }),
+})
+
+export function Thread() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            newMessage: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="newMessage"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>amix</FormLabel>
+                            <FormControl>
+                                <Input placeholder="What is a good book about research" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                Ready to answer
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button disabled={form.formState.isLoading} type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+}
